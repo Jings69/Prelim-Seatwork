c0f3bcc6a2efbd175387a95a68ef65b289bb55c8
Rodolfo Jose Lanorias <95603-22218523@SERVER2.DOMAIN>
2024-08-19 18:33:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const user = {
+    fname: "jessica",
+    lname: "Cordero",
+    gender: "Female",
+    age: 30
+}
+let val = ""
+let val1 = ""
+let val2 = ""
+//dot notaion 
+
+val = user.fname
+val1 = user.lname
+val2 = user.age
+console.log("i am " + val + " " +  val1 + " and i am " + val2 +"" + "years old")
